3fbcfd13fb289a805865621daac63913b3fa43cf
leejabba <dlgodrhs@gmail.com>
2017-05-18 17:00:46 +0900

    init

--- app/src/main/java/com/heythisway/helloandroid/MainActivity.java
+++ app/src/main/java/com/heythisway/helloandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.heythisway.helloandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,11 +12,10 @@ import android.widget.TextView;
  * 2. 객체내에서 변수로 생성
  * 3. setOnclickListener 함수에 익명 개체로 전달(식별자가 없는 객체)
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tv;
     Button btn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.textView);
         btn = (Button) findViewById(R.id.btnClick);
 
-
+        //1번 형태로 구현
+        btn.setOnClickListener(this);
 
         /*
         // 2번 형태로 구현- 아래에 구현된 리스너를 통해 구현된다.
@@ -47,5 +48,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         */
+
+
+    }
+
+    //1번 형태로 구현
+    @Override
+    public void onClick(View v) {
+        tv.setText("안녕 안드로이드");
     }
 }
